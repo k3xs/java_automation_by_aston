d855ac9f281fd45325ccdd4bf0c335ee86595b55
Stas Krasilnikov <kexs888@ya.ru>
2026-03-16 22:01:39 +0500

    add lesson 3 task 1

--- src/Lessson3/Lesson3_task1.java
+++ src/Lessson3/Lesson3_task1.java
@@ -1,8 +1,18 @@
+//    Написать метод, принимающий на вход два целых числа и проверяющий,
+//    что их сумма лежит в пределах от 10 до 20 (включительно),
+//    если да – вернуть true, в противном случае – false.
 package Lessson3;
+
 
 public class Lesson3_task1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println(sumChecker(5, 10));
+        System.out.println(sumChecker(20, 10));
+        System.out.println(sumChecker(0, 10));
+    }
+
+    public static boolean sumChecker(int n1, int n2) {
+        int sum = n1 + n2;
+        return (sum >= 10 && sum <= 20);
     }
 }
